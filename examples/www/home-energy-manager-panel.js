c21d600256841d781297e4f632a470af89e70822
ieko city <icpiot100@gmail.com>
2026-08-04 23:31:54 +1000

    Make theme selection optimistic v237

--- examples/www/home-energy-manager-panel.js
+++ examples/www/home-energy-manager-panel.js
@@ -2,7 +2,7 @@ import "./home-energy-manager-policy-card.js?v=008";
 import "./home-energy-manager-report-card.js?v=302";
 import "./home-energy-manager-debug-card.js?v=035";
 
-const HOME_ENERGY_MANAGER_PANEL_BUILD = "236";
+const HOME_ENERGY_MANAGER_PANEL_BUILD = "237";
 const HOME_ENERGY_MANAGER_PANEL_THEME_KEY = "home-energy-manager.panel.theme";
 const HOME_ENERGY_MANAGER_PANEL_PAGE_KEY = "home-energy-manager.panel.page";
 const HOME_ENERGY_MANAGER_PANEL_PAGE_FRAGMENT_KEY = "hem_page";
@@ -382,6 +382,10 @@ class HomeEnergyManagerPanel extends HTMLElement {
     if (!this._hass) {
       return;
     }
+    this._config = {
+      ...this._config,
+      theme,
+    };
     this._hass.callService("home_energy_manager", "set_panel_theme", {
       entry_id: this._entryId(),
       panel_theme: theme,
